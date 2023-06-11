ddba51ae4b4564d03e3b1223c38b06a026cd7f3a
nikhil3113 <nikhilgovindchavan@ternaengg.ac.in>
2023-06-11 23:16:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [data,setData] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
+  const url = `https://pokeapi.co/api/v2/pokemon/${searchTerm.toLowerCase()}`;
+
+
+  const searchPokemon = (event) =>{
+    if(event.key === "Enter"){
+      axios
+      .get(url)
+      .then((response) =>{
+        setData(response.data);
+        console.log(response.data)
+      })
+      .catch((error) =>{
+        console.log(error);
+      })
+      setSearchTerm('')
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='search'>
+        <input 
+        className='search1'
+          type='text'
+          placeholder='Enter Pokemon name'
+          value={searchTerm}
+          onChange={event => setSearchTerm(event.target.value)}
+          onKeyPress={searchPokemon}
+        />
+      </div>
+     { data ? (<div className='details'>
+        <h1 className='name'>{data.name}</h1>
+        <img className='image' src={data.sprites.front_default} alt={data.name} />
+        <p className='height'>Height: {data.height} </p>
+        <p className='weight'>Weight: {data.weight} </p>
+      </div>
+     ) : (
+      <h1>Pokemon not found</h1>
+    ) }
     </div>
   );
 }
